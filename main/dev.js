6f33c6641a0d92b7d4b51f703c78a79dead3eacf
Thiago Trannin <51510921+thinety@users.noreply.github.com>
2021-05-19 01:29:16 -0300

    Fix executable not working

--- main/dev.js
+++ main/dev.js
@@ -1,20 +1,9 @@
 const fs = require('fs/promises');
 const path = require('path');
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, ipcMain, BrowserWindow } = require('electron');
 
 app.on('ready', () => {
-  const window = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      contextIsolation: true,
-      sandbox: true,
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
-  window.loadURL('http://localhost:3000');
-
-  const cattleDataFile = 'data.json';
+  const cattleDataFile = path.join(__dirname, '../data.json');
   ipcMain.handle('get-cattle-data', async (_event) => {
     const cattleDataBuffer = await fs.readFile(cattleDataFile);
     const cattleData = JSON.parse(cattleDataBuffer);
@@ -24,4 +13,16 @@ app.on('ready', () => {
     const cattleDataString = JSON.stringify(cattleData);
     await fs.writeFile(cattleDataFile, cattleDataString);
   });
+
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      sandbox: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  window.loadURL('http://localhost:3000');
 });
--- main/prod.js
+++ main/prod.js
@@ -1,29 +1,8 @@
 const fs = require('fs/promises');
 const path = require('path');
-const { app, BrowserWindow, protocol, ipcMain } = require('electron');
+const { app, ipcMain, protocol, BrowserWindow } = require('electron');
 
 app.on('ready', () => {
-  const window = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      contextIsolation: true,
-      sandbox: true,
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
-  window.loadFile(path.join(__dirname, '../renderer/out/index.html'));
-
-  protocol.interceptFileProtocol('file', (request, callback) => {
-    let filepath = (new URL(request.url)).pathname;
-
-    if (filepath.startsWith('/_next')) {
-      filepath = path.join(__dirname, '../renderer/out', filepath);
-    }
-
-    callback(filepath);
-  });
-
   const cattleDataFile = path.join(process.env.PORTABLE_EXECUTABLE_DIR, 'data.json');
   ipcMain.handle('get-cattle-data', async (_event) => {
     const cattleDataBuffer = await fs.readFile(cattleDataFile);
@@ -34,4 +13,27 @@ app.on('ready', () => {
     const cattleDataString = JSON.stringify(cattleData);
     await fs.writeFile(cattleDataFile, cattleDataString);
   });
+
+  protocol.interceptFileProtocol('file', (request, callback) => {
+    const requestUrl = new URL(request.url);
+    let filepath = requestUrl.pathname;
+
+    if (filepath.substring(3).startsWith('/_next')) {
+      filepath = path.join(__dirname, '../renderer/out', filepath.substring(4));
+    }
+
+    callback(filepath);
+  });
+
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      sandbox: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  window.loadFile(path.join(__dirname, '../renderer/out/index.html'));
 });
